4100bd8ea90f41f361525cae2575497a60ec3a59
BuiThanhKhai <baika0769@gmail.com>
2026-03-29 13:35:44 +0700

    .

--- Food_Ordering/shipper/shipper.xaml.cs
+++ Food_Ordering/shipper/shipper.xaml.cs
@@ -1,57 +1,48 @@
 ﻿using Food_Ordering.Entities;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Food_Ordering.shipper
 {
-
-    public partial class shipper : Window
+    public partial class Shipper : Window
     {
+        private readonly string _connectionString;
 
-        private readonly string _connectionString = string.Empty;
-        private int _selectedEmployeeId = 0;
-        public shipper()
+        public Shipper()
         {
             InitializeComponent();
             _connectionString = GetConnectionString();
-            LoadEmployees();        }
-
+            LoadOrders();
+        }
 
         private string GetConnectionString()
         {
             IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
+                .SetBasePath(AppContext.BaseDirectory)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .Build();
 
-            return configuration.GetConnectionString("DefaultConnection")!;
+            string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new Exception("DefaultConnection not found in appsettings.json");
+            }
+
+            return connectionString;
         }
 
-
-         
-
-        private void LoadEmployees()
+        private void LoadOrders()
         {
             using FoodOrderingDbContext context = new FoodOrderingDbContext(_connectionString);
             dgOrders.ItemsSource = context.Orders
-                                             .OrderBy(e => e.OrderId)
-                                             .ToList();
+                                          .OrderBy(e => e.OrderId)
+                                          .ToList();
         }
-
-
 
         private void ClearInput()
         {
@@ -60,29 +51,21 @@ namespace Food_Ordering.shipper
             txtTotalAmount.Text = string.Empty;
             txtDeliveryAddress.Text = string.Empty;
             txtNote.Text = string.Empty;
-
             dgOrders.SelectedIndex = -1;
         }
 
         private void dgOrders_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (dgOrders.SelectedItem == null)
+            if (dgOrders.SelectedItem is not Order selectedOrder)
             {
-                ClearInput();
                 return;
             }
 
-            dynamic selectedOrder = dgOrders.SelectedItem;
-
-            txtOrderId.Text = selectedOrder.OrderId?.ToString() ?? "";
-            txtRestaurantId.Text = selectedOrder.RestaurantId?.ToString() ?? "";
-            txtTotalAmount.Text = selectedOrder.TotalAmount?.ToString() ?? "";
-            txtDeliveryAddress.Text = selectedOrder.DeliveryAddress?.ToString() ?? "";
-            txtNote.Text = selectedOrder.Note?.ToString() ?? "";
+            txtOrderId.Text = selectedOrder.OrderId.ToString();
+            txtRestaurantId.Text = selectedOrder.RestaurantId.ToString();
+            txtTotalAmount.Text = selectedOrder.TotalAmount.ToString();
+            txtDeliveryAddress.Text = selectedOrder.DeliveryAddress ?? string.Empty;
+            txtNote.Text = selectedOrder.Note ?? string.Empty;
         }
     }
-
-
-
-
 }
