32c55dbe554e11c2d910548ae02ad6b76a641372
quangquac <quang28102003@gmail.com>
2026-03-29 10:28:00 +0700

    .

--- Food_Ordering/Entities/FoodOrderingDbContext.cs
+++ Food_Ordering/Entities/FoodOrderingDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Food_Ordering.Entities;
 
@@ -39,9 +40,26 @@ public partial class FoodOrderingDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=12345;database=FoodOrderingDB;TrustServerCertificate=True;");
+
+    private string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+             .SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .Build();
+        var strConn = config["ConnectionStrings:DefaultConnection"];
+
+        return strConn;
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=12345;database=FoodOrderingDB;TrustServerCertificate=True;");
+    {
+        optionsBuilder.UseSqlServer(GetConnectionString());
+    }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
